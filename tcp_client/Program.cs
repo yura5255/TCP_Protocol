8cc0a9b2e4ca358fb57f5acd6c16c0a1f1d19414
Bort_lx45@student.itstep.org <Bort_lx45@student.itstep.org>
2023-04-05 07:26:14 -0800

    Add project files.

---
+++ tcp_client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+
+namespace tcp_client
+{
+    class Program
+    {
+        // адрес и порт сервера, к которому будем подключаться
+        static int port = 8080; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        static void Main(string[] args)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+            TcpClient client = new TcpClient();
+
+            // подключаемся к удаленному хосту
+            client.Connect(ipPoint);
+
+            string message = "";
+            try
+            {
+                while (message != "end")
+                {
+                    Console.Write("Enter a message:");
+                    message = Console.ReadLine();
+
+                    NetworkStream ns = client.GetStream();
+
+                    StreamWriter sw = new StreamWriter(ns);
+                    sw.WriteLine(message);//Hello
+
+
+                    sw.Flush(); // send all buffered data and clear the buffer // 1Kb
+
+                    // получаем ответ
+
+                    StreamReader sr = new StreamReader(ns);
+                    string response = sr.ReadLine();
+
+                    Console.WriteLine("server response: " + response);
+
+                    // закриваємо потокі
+                    //sw.Close();
+                    //sr.Close();
+                    //ns.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // закрываем сокет
+                client.Close();
+            }
+        }
+    }
+}
---
+++ tcp_server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tcp_server
+{
+    class Program
+    {
+        static int port = 8080; // порт для приема входящих запросов
+        static void Main(string[] args)
+        {
+            // получаем адреса для запуска сокета
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");//Dns.GetHostEntry("localhost").AddressList[1]; //localhost
+            IPEndPoint ipPoint = new IPEndPoint(iPAddress, port);
+            TcpListener listener = new TcpListener(ipPoint); // bind
+
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+
+                // начинаем прослушивание
+                listener.Start(10);
+
+                Console.WriteLine("Server started! Waiting for connection...");
+                TcpClient client = listener.AcceptTcpClient();
+
+                while (client.Connected)
+                { 
+
+                    NetworkStream ns = client.GetStream();
+
+                    StreamReader sr = new StreamReader(ns);
+                    string response = sr.ReadLine();
+
+                    Console.WriteLine($"{client.Client.RemoteEndPoint} - {response} at {DateTime.Now.ToShortTimeString()}");
+
+                    // отправляем ответ
+                    string message = "Message was send!";
+
+                    StreamWriter sw = new StreamWriter(ns);
+                    sw.WriteLine(message);
+
+                    sw.Flush();
+
+                    // закриваємо потокі
+                    //sr.Close();
+                    //sw.Close();
+                    //ns.Close();
+                }
+                // закрываем сокет
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            listener.Stop();
+        }
+    }
+}
